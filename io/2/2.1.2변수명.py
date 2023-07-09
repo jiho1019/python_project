50a98911ea001892c71af1449e66a7c4206761b9
askpython1022 <askpyton1022@gmail.com>
2023-07-09 15:45:45 +0900

    :memo:update

---
+++ io/2/2.1.2변수명.py
@@ -0,0 +1,19 @@
+num1 = 15 # num1 변수에 15를 대입
+num2 = 12 # num2 변수에2를 대입
+result = num1 - num2 # result에 num1 - num2를 대입
+print(result) # result를 출력
+
+school = "한국대학교" # school 변수에 한국대학교를 대입
+print(school) # school을 출력
+
+#24cafe = "커피"
+#변수명의 앞에 숫자를 쓰면 에러 발생
+
+input_name = "김영진" #input_name 함수에 김영진을대입
+math_score = 90 #math_score 함수에 90을 대입
+screenColor = "red" #screenColor함수에 "red"를 대입
+windowHeight = 1000 #windowHeight함수에 1000을 대입
+
+#email@ = "user@korea.com"
+#fornt size = 16
+#특수문자와 공백은 변수명에사용 불가
